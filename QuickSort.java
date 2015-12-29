9ce7f11348bd81c8e3fdc3ee1c952b4f5f314878
orhuncirakli <orhuncirakli@users.noreply.github.com>
2015-12-29 17:22:21 +0200

    QuickSort.java

---
+++ QuickSort.java
@@ -0,0 +1,27 @@
+	public static void quickSort(int[] array, int left, int right){
+		int i = left;
+		int j = right;
+		int temp = 0;
+		int pivot = array[(left + right) / 2];
+		while(i<=j){
+			while(array[i]<pivot){
+				i++;
+			}
+			while(array[j]>pivot){
+				j--;
+			}
+			if(i<=j){
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				i++;
+				j--;
+			}			
+		}
+		if(left < j){
+			quickSort(array, left, j);
+		}
+		if(right > i){
+			quickSort(array, i, right);
+		}
+	}

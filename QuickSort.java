1c9ac38cc411e20e0daa918a0071ddc5d854f699
orhuncirakli <orhuncirakli@users.noreply.github.com>
2016-01-01 22:08:08 +0200

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -1,6 +1,6 @@
 	public static void quickSort(int[] array, int left, int right){
-		int l = left;
-		int r = right;
+		int i = left;
+		int j = right;
 		int temp = 0;
 		int pivot = array[(left + right) / 2];
 		while(i<=j){
